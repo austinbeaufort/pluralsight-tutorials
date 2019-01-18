e78bbfd3d976e8579d75d9e4819b48907e71604e
Austin Beaufort <tinyhatcoding@gmail.com>
2019-01-17 19:37:23 -0500

    Just started this intro to JS Course from PluralSight!

---
+++ javascript/js-gettingstarted/gettingstarted.js
@@ -0,0 +1,22 @@
+// console.log('hello world');
+
+// multiple variables can be declared at once seperated wtih commas.
+// let productName = 'hammer', // comma to declare multiple variables.
+//     productId = 'h123';
+
+// console.log(productName, productId);
+
+//escaping a character with backslash.
+// let productName = 'John\'s Hammer';
+// console.log(productName);
+
+// let price = 5.00,
+//     price2 = 3.00;
+
+// console.log(price, price2);
+
+// let firstName = 'john',
+//     lastName = 'doe';
+
+// console.log(`${firstName} ${lastName}`);
+// console.log(firstName + " " + lastName);
